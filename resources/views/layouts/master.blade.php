f36204655104ddf70c979f7645c0e217b323d8f8
Razak Syaiful Rochman <razak.syaiful.r@gmail.com>
2023-01-18 11:08:42 +0700

    blading

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -22,10 +22,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
     <link rel="stylesheet" href="{{asset('assets/css/colors.php?color=f7c25e')}}" type="text/css"/>
-
+    @stack('css')
     <!-- Document Title
     ============================================= -->
-    <title>KORPRI KABUPATEN SITUBONDO</title>
+    @yield('title')
 
 </head>
 
@@ -51,10 +51,7 @@
                                 src="demos/freelancer/images/logo@2x.png" alt="Canvas Logo"></a>
                     </div><!-- #logo end -->
 
-                    <div class="header-misc">
-                        <a href="mailto:noreply@canvas.com" class="button button-border rounded-pill">Email Us</a>
-                    </div>
-
+                
                     <div id="primary-menu-trigger">
                         <svg class="svg-trigger" viewBox="0 0 100 100">
                             <path
@@ -108,50 +105,13 @@
 
     <!-- Slider
     ============================================= -->
-    <section id="slider" class="slider-element min-vh-md-100 py-4 include-header"
-             style="background: #FFF url('demos/freelancer/images/hero-bg.svg') repeat top center; background-size: cover;">
-        <div class="slider-inner">
-            <div class="vertical-middle slider-element-fade">
-                <div class="container text-center py-5">
-                    <div class="emphasis-title mb-2">
-                        <h4 class="text-uppercase ls3 fw-bolder mb-0">Selamat Datang di Website Kami</h4>
-                        <h1>
-								<span id="oc-images" class="owl-carousel image-carousel carousel-widget" data-items="1"
-                                      data-margin="0" data-autoplay="3000" data-loop="true" data-nav="false"
-                                      data-pagi="false" data-animate-in="fadeInUp">
-									<div
-                                        class="oc-item gradient-text gradient-red-yellow text-uppercase">KORPRI</div>
-									<div class="oc-item gradient-text gradient-red-yellow text-uppercase">KABUPATEN</div>
-									<div class="oc-item gradient-text gradient-red-yellow text-uppercase">SITUBONDO</div>
-								</span>
-                        </h1>
-                    </div>
-                    <!-- <div class="d-flex align-items-center justify-content-center mb-5">
-                        <img src="demos/freelancer/images/face.svg" alt="Face" width="60">
-                        <span class="text-uppercase fw-bold ms-3">SemiColonWeb</span>
-                    </div> -->
-                    <div class="mx-auto" style="max-width: 600px">
-                        <p class="lead fw-normal text-dark mb-5">Authoritatively expedite backward-compatible e-commerce
-                            with cross-media e-commerce. Credibly provide access to world-class action items through
-                            resource-leveling resources.</p>
-                        <a href="demo-freelancer-works.html"
-                           class="button button-dark button-hero h-translatey-3 tf-ts button-reveal overflow-visible bg-dark text-end"><span>View our Works</span><i
-                                class="icon-line-arrow-right"></i></a>
-                        <a href="#" data-scrollto="#footer" data-easing="easeInOutExpo" data-speed="1250"
-                           data-offset="70" class="button button-large button-light text-dark bg-transparent m-0"><i
-                                class="icon-line2-arrow-down fw-bold"></i> <u>Contact Me</u></a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section><!-- #slider end -->
-
+   
     <!-- Content
     ============================================= -->
     <section id="content">
 
         <div class="content-wrap p-0">
-
+        @yield('content')
         </div>
 
     </section><!-- #content end -->
@@ -258,6 +218,7 @@
 <!-- Footer Scripts
 ============================================= -->
 <script src="{{asset('assets/js/functions.js')}}"></script>
+@stack('js')
 
 <script>
     // Owl Carousel Scripts
